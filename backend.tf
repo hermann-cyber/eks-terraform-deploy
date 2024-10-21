de39b7c2dc04e4ce06ffabf68d6bb2a1aa5bca97
dampes <ngwahermann@yahoo.com>
2024-10-22 02:15:29 +0800

    eks file modified

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "infra/terraform.state"
-    bucket         = "dominion-terraform-backend-bucket"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-state-locking"
+    bucket         = "c36-terraform-backend-bucket"
+    region         = "us-east-2"
+    dynamodb_table = "c36-terraform-state-locking"
   }
 }
